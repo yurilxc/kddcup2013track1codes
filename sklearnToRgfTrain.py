0ef765993ebf9bb2e45aea6b42c269709e3037be
arkchar <yurilxc@gmail.com>
2013-05-08 01:15:35 +0800

    add main process of sklearnToRgfTrain.py

--- sklearnToRgfTrain.py
+++ sklearnToRgfTrain.py
@@ -30,3 +30,24 @@ def parseArgs():
 
 if __name__ == "__main__":
     args = parseArgs()
+    ifile = args.input
+    ofilex = open(args.outputPrefix + '.x', 'w')
+    ofiley = open(args.outputPrefix + '.y', 'w')
+    featureSize = -1
+    for line in ifile:
+        label, features = line.strip().split('\t')
+        if label == "0":
+            label = "-1"
+        else:
+            label = "1"
+        features = features.split(',')
+        if featureSize == -1:
+            featureSize = len(features)
+            ofilex.write(str(featureSize) + '\n')
+        for i, feature in enumerate(features):
+            features[i] = str(i) + ':' + feature
+        ofilex.write(' '.join(features) + '\n')
+    ifile.close()
+    ofilex.close()
+    ofiley.close()
+
