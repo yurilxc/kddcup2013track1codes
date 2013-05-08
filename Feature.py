b5bc2062e173a8c51f89fc7c465a4687be774729
arkchar <yurilxc@gmail.com>
2013-05-08 20:19:44 +0800

    add toString

--- Feature.py
+++ Feature.py
@@ -3,7 +3,19 @@ class Feature:
         """line: [(0, 1), (7, 2.3)]"""
         self.__size = size
         self.__lines = lines
-        
+
+    def toString(self):
+        """return:
+        sparse\t8
+        0:1 1:3 7:8
+        1:2 3:2.3 7:3.0
+        ..."""
+        s = "sparse\t{:d}\n".format(self.size)
+        for line in self.lines:
+            line = map(lambda x: str(x[0]) + ':' + str(x[1]), line)
+            s += ' '.join(line) + '\n'
+        return s
+
     @property
     def size(self):
         return self.__size
