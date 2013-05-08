8303cefa36871dbf227bb446747cbe68a69d1aaa
arkchar <yurilxc@gmail.com>
2013-05-08 20:49:43 +0800

    add fromString

--- Feature.py
+++ Feature.py
@@ -16,6 +16,22 @@ class Feature:
             s += ' '.join(line) + '\n'
         return s
 
+    @staticmethod
+    def fromSting(s):
+        """input:
+        sparse\t8
+        0:1 1:3 7:8
+        1:2 3:2.3 7:3.0
+        ..."""
+        size = int(s.split('\n')[0].strip()[len('sparse'):])
+        lines = []
+        for i, line in enumerate(s.split('\n')[1:]):
+            line = line.split()
+            line = map(lambda x: x.split(':'), line)
+            line = map(lambda x: (int(x[0]), float(x[1])), line)
+            lines.append(line)
+        return Feature(size, lines)
+        
     @property
     def size(self):
         return self.__size
